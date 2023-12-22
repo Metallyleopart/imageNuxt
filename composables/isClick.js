0d5e2036ea64fa1e7ccf418e657e812a505d1b86
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-12-22 22:51:30 +0700

    fix svg

--- composables/isClick.js
+++
@@ -1,9 +0,0 @@
-export default function isClick() {
-  const { data:clickImage, pending, error, execute } = useFetch(``);
-
-  function isClick(params) {
-    execute()
-  }
-
-  return {clickImage, isClick}
-}
