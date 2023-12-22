3a9b6aaabd597541d0f9ecd23340ea76cb6a1321
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-12-22 22:10:14 +0700

    fix icon

--- components/loading.vue
+++ components/loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center fill-gray-800 text-gray-800">
-    <NuxtImg class="mr-2 w-5 h-5 animate-spin" src="./public/svg/loading.svg" alt="loading icon" />
+    <NuxtImg class="mr-2 w-5 h-5 animate-spin" src="../public/svg/download.svg" alt="loading icon" />
     <span>loading...</span>
   </div>
 </template>
