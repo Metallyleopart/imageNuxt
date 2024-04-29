450f4ae33328e3c33263fb40bf7af50231755c0b
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-29 23:53:26 +0700

    add loading in download

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@
     <div v-if="datas" class="h-full w-full">
       <ul class="columns-xs w-full mt-10 gap-4">
         <li v-for="data in datas" :key="data" class="tes-2 flex relative mb-4 rounded-md overflow-hidden cursor-pointer">
-          <nuxt-img @click="selectImage(data)" :src="`${data?.src?.large2x}`" :alt="`${data?.alt}`" placeholder placeholder-class="blur" loading="lazy"/>
+          <nuxt-img @click="selectImage(data)" :src="`${data?.src?.large2x}`" :alt="`${data?.alt}`" placeholder placeholder-class="blur" loading="lazy" />
         </li>
       </ul>
       <div v-if="isOpen" class="fixed z-10 inset-0 overflow-y-auto bg-gray-500 bg-opacity-75 transition-opacity">
@@ -29,11 +29,16 @@
               </h4>
               <div class="mt-1">
                 <p class="text-sm text-gray-500">Click the button below to download the image.</p>
-                <NuxtImg :src="selectedImage.src.large2x" :alt="selectedImage.alt" class="mt-4" />
+                <NuxtImg class="mx-auto mt-4" :src="selectedImage.src.large2x" :alt="selectedImage.alt" />
               </div>
             </div>
             <div class="bg-gray-50 py-3 px-6 flex flex-col gap-y-2 md:px-4 md:flex-row-reverse md:gap-x-2">
-              <button type="button" class="block px-4 py-2 text-white text-base bg-blue-dark w-full outline-none rounded-md md:inline-flex md:justify-center md:font-medium" @click="downloadImage()">Download</button>
+              <div v-if="isLoading == true">
+                <loading />
+              </div>
+              <div class="block px-4 py-2 text-white text-base bg-blue-dark w-full outline-none rounded-md md:inline-flex md:justify-center md:font-medium" v-else>
+                <button type="button" @click="downloadImage() & (isLoading == true)">Download</button>
+              </div>
               <button type="button" class="block px-4 py-2 text-base bg-white w-full border-2 rounded-md md:inline-flex md:justify-center md:font-medium" @click="isOpen = false">Cancel</button>
             </div>
           </div>
@@ -67,6 +72,7 @@
   const isOpen = ref(false);
   const isMore = ref(false);
   const selectedImage = ref(null);
+  const isLoading = ref(false);
 
   // import dengan menggunakan object dan nama file
   const { input, datas, pending, error, fetchData, loadMore } = useFetchData();
@@ -77,15 +83,19 @@
     console.log(selectedImage);
   }
 
-  function downloadImage() {
-    fetch(selectedImage.value.src.original)
-      .then((res) => res.blob())
-      .then((blob) => {
-        const a = document.createElement('a');
-        a.href = URL.createObjectURL(blob);
-        a.download = selectedImage.value.alt;
-        a.click();
-      })
-      .catch(() => alert('Failed download image'));
+  async function downloadImage() {
+    isLoading.value = true;
+    try {
+      const res = await fetch(selectedImage.value.src.original);
+      const blob = await res.blob();
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      a.download = selectedImage.value.alt + ' ' + selectedImage.value.photographer + ' ' + 'Unsplash';
+      a.click();
+    } catch (error) {
+      alert('Failed download image');
+    } finally {
+      isLoading.value = false;
+    }
   }
 </script>
