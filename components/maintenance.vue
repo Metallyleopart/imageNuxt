9251a8c3a0f093359aa4a02e57472a050828173f
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-06-11 23:14:49 +0700

    add maintenence

---
+++ components/maintenance.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="absolute w-full h-full bg-slate-300/50">
+    <span class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-xl font-semibold">Maintenance</h1>
+      <h2>We're sorry</h2>
+    </span>
+  </div>
+</template>
